8d1886be1761fce8fa91f73ef4edf16526328eab
rkdev <rkdevmail@gmail.com>
2017-09-13 18:14:08 -0500

    added attribution for destroy_docker

--- utils/destroy_docker.sh
+++ utils/destroy_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#https://davidwalsh.name/docker-remove-all-images-containers
+
 # Delete every Docker containers
 # Must be run first because images are attached to containers
 docker rm -f $(docker ps -a -q)
